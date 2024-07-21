9fbca44cf98455d83d49b8e44094f3e5eaa80b3b
Dan Blair <s223215521@deakin.edu.au>
2024-07-21 22:26:35 +1000

    Reverted back

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -8,10 +8,4 @@ describe("video streaming microservice", () => {
         const response = await request(app).get("/live"); // Makes a request to the "/live" route.
         expect(response.status).toBe(200); // Verify that a HTTP status code 200 is returned, indicating success.
     });
-
-    test("microservice can handle another request", async () => {
-
-        const response = await request(app).get("/video"); // Makes a request to the "/live" route.
-        expect(response.status).toBe(200); // Verify that a HTTP status code 200 is returned, indicating success.
-    });
 });
